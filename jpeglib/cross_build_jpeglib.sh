12122379da2f4c1be75d5ffe187e6ad21196e53f
chenxin32 <1763420491@qq.com>
2020-05-16 00:27:19 +0800

    交叉编译环境下安装jpeglib

--- jpeglib/cross_build_jpeglib.sh
+++ jpeglib/cross_build_jpeglib.sh
@@ -1,5 +1,6 @@
 sudo apt-get install gcc-arm-linux-gnueabihf
 sudo mkdir /usr/local/jpeg
+sudo rm -rf ./jpeg-9d/
 tar -zvxf jpegsrc.v9d.tar.gz
 cd ./jpeg-9d
 ./configure CC=arm-linux-gnueabihf-gcc LD=arm-linux-gnueabihf-ld --host=arm-linux-gnueabihf --prefix=/usr/local/jpeg --enable-shared --enable-static
